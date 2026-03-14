41b660c647b3d97b9f45f0cf9ba56da592387639
Samuel Furlanich <samu.1furlanich@gmail.com>
2026-03-14 16:28:34 -0300

    Update Navbar.tsx

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -57,7 +57,7 @@ export function Navbar({ name, socialMedia, locale, onLocaleChange }: NavbarProp
           <div className="hidden items-center gap-4 md:flex">
             <div className="flex items-center gap-2">
               {socialMedia.map((item) => (
-                <IconButton key={item.url} href={item.url} label={item.alt}>
+                <IconButton key={item.url} href={item.url} label={item.image.alt}>
                   <img
                     src={withBasePath(item.image.path)}
                     alt={item.image.alt}
@@ -93,7 +93,7 @@ export function Navbar({ name, socialMedia, locale, onLocaleChange }: NavbarProp
             ))}
             <div className="flex items-center gap-3 pt-2">
               {socialMedia.map((item) => (
-                <IconButton key={item.url} href={item.url} label={item.alt}>
+                <IconButton key={item.url} href={item.url} label={item.image.alt}>
                   <img
                     src={withBasePath(item.image.path)}
                     alt={item.image.alt}
